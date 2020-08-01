ca1796ced50107d659789bab166ce4d67548459e
siddharthdayalwal <siddharthdayalwal2000@gmail.com>
2020-08-01 17:47:05 +0530

    Added game_opening function

--- game.py
+++ game.py
@@ -27,3 +27,16 @@ o_img = pg.image.load('o.png')
 x_img = pg.transform.scale(x_img, (80,80))
 o_img = pg.transform.scale(o_img, (80,80))
 opening = pg.transform.scale(opening, (width, height+100))
+
+def game_opening():
+    screen.blit(opening,(0,0))
+    pg.display.update()
+    time.sleep(1)
+    screen.fill(white)
+    # Drawing vertical lines
+    pg.draw.line(screen,line_color,(width/3,0),(width/3, height),7)
+    pg.draw.line(screen,line_color,(width/3*2,0),(width/3*2, height),7)
+    # Drawing horizontal lines
+    pg.draw.line(screen,line_color,(0,height/3),(width, height/3),7)
+    pg.draw.line(screen,line_color,(0,height/3*2),(width, height/3*2),7)
+    draw_status()
