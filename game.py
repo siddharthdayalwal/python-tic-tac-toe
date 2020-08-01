d727d2fbe4e6a764f2628ef1a488d24c4cf0d370
siddharthdayalwal <siddharthdayalwal2000@gmail.com>
2020-08-01 17:33:15 +0530

    Created new window

--- game.py
+++ game.py
@@ -11,3 +11,10 @@ white = (255, 255, 255)
 line_color = (10,10,10)
 #TicTacToe 3x3 board
 TTT = [[None]*3,[None]*3,[None]*3]
+
+#initializing pygame window
+pg.init()
+fps = 30
+CLOCK = pg.time.Clock()
+screen = pg.display.set_mode((width, height+100),0,32)
+pg.display.set_caption("Tic Tac Toe")
