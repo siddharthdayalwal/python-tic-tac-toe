c156fcd5685077a42d8dcc0a3d56c0c660335748
siddharthdayalwal <siddharthdayalwal2000@gmail.com>
2020-08-01 18:24:15 +0530

    Added drawXO function which will show X and O when clicked

--- game.py
+++ game.py
@@ -89,3 +89,28 @@ def check_win():
     if(all([all(row) for row in TTT]) and winner is None ):
         draw = True
     draw_status()
+
+def drawXO(row,col):
+    global TTT,XO
+    if row==1:
+        posx = 30
+    if row==2:
+        posx = width/3 + 30
+    if row==3:
+        posx = width/3*2 + 30
+    if col==1:
+        posy = 30
+    if col==2:
+        posy = height/3 + 30
+    if col==3:
+        posy = height/3*2 + 30
+TTT[row-1][col-1] = XO
+    if(XO == 'x'):
+        screen.blit(x_img,(posy,posx))
+XO= 'o'
+else:
+        screen.blit(o_img,(posy,posx))
+        XO= 'x'
+    pg.display.update()
+    #print(posx,posy)
+    #print(TTT)
