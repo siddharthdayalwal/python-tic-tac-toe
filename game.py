b55b42836fffb441eeadb1b5a32f8aff2b5ff68b
siddharthdayalwal <siddharthdayalwal2000@gmail.com>
2020-08-01 18:33:51 +0530

    Added reset_game function to restart game

--- game.py
+++ game.py
@@ -142,3 +142,12 @@ def userClick():
         #draw the x or o on screen
         drawXO(row,col)
         check_win()
+
+def reset_game():
+    global TTT, winner,XO, draw
+    time.sleep(3)
+    XO = 'x'
+    draw = False
+    game_opening()
+    winner=None
+    TTT = [[None]*3,[None]*3,[None]*3]
