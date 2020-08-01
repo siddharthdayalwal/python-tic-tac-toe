9fa0b6c840a5ae961ad326e86347c047b41ef175
siddharthdayalwal <siddharthdayalwal2000@gmail.com>
2020-08-01 17:50:28 +0530

    Added draw_status function which updates turn of a player

--- game.py
+++ game.py
@@ -40,3 +40,21 @@ def game_opening():
     pg.draw.line(screen,line_color,(0,height/3),(width, height/3),7)
     pg.draw.line(screen,line_color,(0,height/3*2),(width, height/3*2),7)
     draw_status()
+
+def draw_status():
+    global draw
+    if winner is None:
+        message = XO.upper() + "'s Turn"
+    else:
+        message = winner.upper() + " won!"
+    if draw:
+        message = 'Game Draw!'
+    font = pg.font.Font(None, 30)
+    text = font.render(message, 1, (255, 255, 255))
+    # copy the rendered message onto the board
+    screen.fill ((0, 0, 0), (0, 400, 500, 100))
+    text_rect = text.get_rect(center=(width/2, 500-50))
+    screen.blit(text, text_rect)
+    pg.display.update()
+
+    
