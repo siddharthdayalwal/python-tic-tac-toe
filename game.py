20d4dfea6e90a67d35f68d3b8eb45f52bd4c5791
siddharthdayalwal <siddharthdayalwal2000@gmail.com>
2020-08-01 18:32:12 +0530

    Added userclick function

--- game.py
+++ game.py
@@ -114,3 +114,31 @@ else:
     pg.display.update()
     #print(posx,posy)
     #print(TTT)
+
+def userClick():
+    #get coordinates of mouse click
+    x,y = pg.mouse.get_pos()
+    #get column of mouse click (1-3)
+    if(x<width/3):
+        col = 1
+    elif (x<width/3*2):
+        col = 2
+    elif(x<width):
+        col = 3
+    else:
+        col = None
+    #get row of mouse click (1-3)
+    if(y<height/3):
+        row = 1
+    elif (y<height/3*2):
+        row = 2
+    elif(y<height):
+        row = 3
+    else:
+        row = None
+    #print(row,col)
+    if(row and col and TTT[row-1][col-1] is None):
+        global XO
+        #draw the x or o on screen
+        drawXO(row,col)
+        check_win()
